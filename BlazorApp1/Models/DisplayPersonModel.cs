5d31f9b1679e47725c4dc33f2e103c61c47c7b08
elvis_makia <elvis_makia@yahoo.com>
2020-12-13 05:31:47 +0000

    Add project files.

---
+++ BlazorApp1/Models/DisplayPersonModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(13, ErrorMessage = "First Name is too Long")]
+        [MinLength(5, ErrorMessage = "First Name is too  short")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(13, ErrorMessage = "Last Name is too Long")]
+        [MinLength(5, ErrorMessage = "Last Name is too  short")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
